7f3487861f156fcaeb8ff321cd63122a3ad64dea
Filipe Ramalho Fernandes <filipe.ramalhofernandes@gmail.com>
2018-12-15 18:58:52 +0000

    Refactor code

--- Lab6/p2pdata/lab6.py
+++ Lab6/p2pdata/lab6.py
@@ -63,7 +63,7 @@ def index(filename, mode, percentage):
         if ip not in list_ip:
             list_ip.append(ip)
             if p2p == "p2p":
-                dic_ips.update({ip: (1,0)})
+                dic_ips.update({ip: (1, 0)})
             else:
                 dic_ips.update({ip: (0, 1)})
         else:
@@ -79,70 +79,70 @@ def index(filename, mode, percentage):
         if connection not in list_connection:
             list_connection.append(connection)
             if p2p == "p2p":
-                dic_connection.update({connection: (1,0)})
+                dic_connection.update({connection: (1, 0)})
             else:
                 dic_connection.update({connection: (0, 1)})
         else:
             tmp = dic_connection.get(connection)
-            if p2p =="p2p":
-                tmp = (tmp[0]+1,tmp[1])
+            if p2p == "p2p":
+                tmp = (tmp[0]+1, tmp[1])
                 dic_connection.update({connection: tmp})
             else:
-                tmp = (tmp[0],tmp[1]+1)
+                tmp = (tmp[0], tmp[1]+1)
                 dic_connection.update({connection: tmp})
 
         # Populate bandwidth dictionary
         if band not in list_bandwidth:
             list_bandwidth.append(band)
-            if p2p =="p2p":
-                dic_bandwidth.update({band: (1,0)})
+            if p2p == "p2p":
+                dic_bandwidth.update({band: (1, 0)})
             else:
                 dic_bandwidth.update({band: (0, 1)})
         else:
             tmp = dic_bandwidth.get(band)
-            if p2p =="p2p":
-                tmp = (tmp[0]+1,tmp[1])
+            if p2p == "p2p":
+                tmp = (tmp[0]+1, tmp[1])
                 dic_bandwidth.update({band: tmp})
             else:
-                tmp = (tmp[0],tmp[1]+1)
+                tmp = (tmp[0], tmp[1]+1)
                 dic_bandwidth.update({band: tmp})
 
         # Populate packet_size dictionary
         if packet_size not in list_packet_size:
             list_packet_size.append(packet_size)
-            if p2p =="p2p":
-                dic_packet_size.update({packet_size: (1,0)})
+            if p2p == "p2p":
+                dic_packet_size.update({packet_size: (1, 0)})
             else:
                 dic_packet_size.update({packet_size: (0, 1)})
         else:
             tmp = dic_packet_size.get(packet_size)
-            if p2p =="p2p":
-                tmp = (tmp[0]+1,tmp[1])
+            if p2p == "p2p":
+                tmp = (tmp[0]+1, tmp[1])
                 dic_packet_size.update({packet_size: tmp})
             else:
-                tmp = (tmp[0], tmp[1]+ 1)
+                tmp = (tmp[0], tmp[1] + 1)
                 dic_packet_size.update({packet_size: tmp})
 
         # Populate time dictionary
         if time not in list_time:
             list_time.append(time)
-            if p2p =="p2p":
-                dic_time.update({time: (1,0)})
+            if p2p == "p2p":
+                dic_time.update({time: (1, 0)})
             else:
                 dic_time.update({time: (0, 1)})
         else:
             tmp = dic_time.get(time)
-            if p2p =="p2p":
-                tmp = (tmp[0]+1,tmp[1])
+            if p2p == "p2p":
+                tmp = (tmp[0]+1, tmp[1])
                 dic_time.update({time: tmp})
             else:
-                tmp = (tmp[0],tmp[1]+1)
+                tmp = (tmp[0], tmp[1]+1)
                 dic_time.update({time: tmp})
 
         if p2p not in list_p2p:
             list_p2p.append(p2p)
             if p2p == "p2p":
-                dic_p2p.update({"count": (1,0)})
+                dic_p2p.update({"count": (1, 0)})
             else:
                 dic_p2p.update({"count": (0, 1)})
         else:
@@ -166,7 +166,8 @@ def get_prob(dic_p2p):
     return prob_p2p, prob_np2p
 
 
-def output(filename, mode, missing_range, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p):
+def output(filename, mode, missing_range, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time,
+           prob_p2p, prob_np2p):
     lines = []
     dfu = pd.read_csv(filename, header=None)
 
@@ -188,8 +189,8 @@ def output(filename, mode, missing_range, dic_ips, dic_connection, dic_bandwidth
         # IP:
         ip_label_ip = dic_ips.get(ip_u)
         if ip_label_ip is not None:
-            IP_p2p = ip_label_ip[0]/prob_p2p
-            IP_np2p = ip_label_ip[1]/prob_np2p
+            ip_p2p = ip_label_ip[0]/prob_p2p
+            ip_np2p = ip_label_ip[1]/prob_np2p
 
         # Connection:
         ip_label_connection = dic_connection.get(connection_u)
@@ -214,18 +215,20 @@ def output(filename, mode, missing_range, dic_ips, dic_connection, dic_bandwidth
             time_p2p = ip_label_time[0] / prob_p2p
             time_np2p = ip_label_time[1] / prob_np2p
 
-        total_p2p = IP_p2p * conn_p2p * band_p2p * packet_p2p * time_p2p * prob_p2p
-        total_np2p = IP_np2p * conn_np2p * band_np2p * packet_np2p * time_np2p * prob_np2p
+        total_p2p = ip_p2p * conn_p2p * band_p2p * packet_p2p * time_p2p * prob_p2p
+        total_np2p = ip_np2p * conn_np2p * band_np2p * packet_np2p * time_np2p * prob_np2p
 
     # Label mode
         if mode == "label":
 
             if total_p2p > total_np2p:
-                line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u + "," + "p2p\n"
+                line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u \
+                       + "," + "p2p\n"
                 lines.append(line)
 
             if total_p2p < total_np2p:
-                line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u + "," + "not p2p\n"
+                line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," \
+                       + time_u + "," + "not p2p\n"
                 lines.append(line)
         else:
             if total_p2p > total_np2p:
@@ -275,16 +278,19 @@ def main():
             print(files_u[count])
             dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p = index(filename, "label", 1)
             prob_p2p, prob_np2p = get_prob(dic_p2p)
-            output(files_u[count], "label", dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p)
+            output(files_u[count], "label", dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time,
+                   prob_p2p, prob_np2p)
             count += 1
 
     # Error mode
     if error is not None:
         for filename in files:
             print(filename)
-            dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p, missing_range = index(filename, "error", float(error))
+            dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p, missing_range = \
+                index(filename, "error", float(error))
             prob_p2p, prob_np2p = get_prob(dic_p2p)
-            output(filename, "error", missing_range, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p)
+            output(filename, "error", missing_range, dic_ips, dic_connection, dic_bandwidth, dic_packet_size,
+                   dic_time, prob_p2p, prob_np2p)
 
     if not label and error is None:
         parser.print_help()
