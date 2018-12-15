fe6a426bd6a53b5c66b66fccf561e5a01ea8163c
Filipe Ramalho Fernandes <filipe.ramalhofernandes@gmail.com>
2018-12-15 18:21:19 +0000

    Update lab6.py

--- Lab6/p2pdata/lab6.py
+++ Lab6/p2pdata/lab6.py
@@ -1,10 +1,33 @@
 import pandas as pd
+import argparse
+import random
 
 files = ["1-labeled.dat", "2-labeled.dat", "3-labeled.dat", "4-labeled.dat"]
 files_u = ["1-unlabeled.dat", "2-unlabeled.dat", "3-unlabeled.dat", "4-unlabeled.dat"]
 
 
-def index(filename):
+def get_parser():
+    parser = argparse.ArgumentParser(description="""Traffic Engineering P2P classification tool""")
+    parser.add_argument('-e', '--error', dest='error', help="""specify training percentage, ex: 0.7""",
+                        action="store")
+    parser.add_argument('-l', '--label', help="""Label the unlabeled files""",
+                        action="store_true")
+    return parser
+
+
+def gen_random(start, end, num):
+    res = []
+
+    for j in range(num):
+        val = random.randint(start, end)
+        while val in res:
+            val = random.randint(start, end)
+        res.append(val)
+
+    return res
+
+
+def index(filename, mode, percentage):
     df = pd.read_csv(filename, header=None)
     dic_ips = {}
     list_ip = []
@@ -19,7 +42,15 @@ def index(filename):
     dic_p2p = {}
     list_p2p = []
 
-    for n in range(len(df)):
+    if mode == "label":
+        mode_range = range(len(df))
+    else:
+        val = int(percentage*len(df))
+        mode_range = gen_random(0, 9999, val)
+        all_range = list(range(len(df)))
+        missing_range = set(all_range).difference(mode_range)
+
+    for n in mode_range:
 
         ip = df.values[n][0]
         connection = df.values[n][1]
@@ -122,7 +153,11 @@ def index(filename):
             else:
                 tmp = (tmp[0], tmp[1] + 1)
                 dic_p2p.update({"count": tmp})
-    return dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p
+
+    if mode == "error":
+        return dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p, missing_range
+    else:
+        return dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p
 
 
 def get_prob(dic_p2p):
@@ -131,15 +166,24 @@ def get_prob(dic_p2p):
     return prob_p2p, prob_np2p
 
 
-def output(filename, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p):
+def output(filename, mode, missing_range, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p):
     lines = []
     dfu = pd.read_csv(filename, header=None)
-    for n in range(len(dfu)):
+
+    if mode == "label":
+        mode_range = range(len(dfu))
+    else:
+        error = 0
+        nr_times = 0
+        mode_range = missing_range
+
+    for n in mode_range:
         ip_u = dfu.values[n][0]
         connection_u = dfu.values[n][1]
         band_u = dfu.values[n][2]
         packet_size_u = dfu.values[n][3]
         time_u = dfu.values[n][4]
+        p2p_u = dfu.values[n][5]
 
         # IP:
         ip_label_ip = dic_ips.get(ip_u)
@@ -173,42 +217,77 @@ def output(filename, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, di
         total_p2p = IP_p2p * conn_p2p * band_p2p * packet_p2p * time_p2p * prob_p2p
         total_np2p = IP_np2p * conn_np2p * band_np2p * packet_np2p * time_np2p * prob_np2p
 
-        if total_p2p > total_np2p:
-            line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u + "," + "p2p\n"
-            lines.append(line)
+    # Label mode
+        if mode == "label":
 
-        if total_p2p < total_np2p:
-            line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u + "," + "not p2p\n"
-            lines.append(line)
+            if total_p2p > total_np2p:
+                line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u + "," + "p2p\n"
+                lines.append(line)
 
-    # Write Output file
-    if filename.startswith("1-"):
-        f = open("out-1-labeled.dat", "w")
-        f.writelines(lines)
-        f.close()
-    if filename.startswith("2-"):
-        f = open("out-2-labeled.dat", "w")
-        f.writelines(lines)
-        f.close()
-    if filename.startswith("3-"):
-        f = open("out-3-labeled.dat", "w")
-        f.writelines(lines)
-        f.close()
-    if filename.startswith("4-"):
-        f = open("out-4-labeled.dat", "w")
-        f.writelines(lines)
-        f.close()
+            if total_p2p < total_np2p:
+                line = ip_u + "," + str(connection_u) + "," + str(band_u) + "," + str(packet_size_u) + "," + time_u + "," + "not p2p\n"
+                lines.append(line)
+        else:
+            if total_p2p > total_np2p:
+                if p2p_u != "p2p":
+                    error = error + 1
+
+            if total_p2p < total_np2p:
+                if p2p_u != "not p2p":
+                    error = error + 1
+            nr_times = nr_times + 1
+
+    if mode == "label":
+        # Write Output file
+        if filename.startswith("1-"):
+            f = open("out-1-labeled.dat", "w")
+            f.writelines(lines)
+            f.close()
+        if filename.startswith("2-"):
+            f = open("out-2-labeled.dat", "w")
+            f.writelines(lines)
+            f.close()
+        if filename.startswith("3-"):
+            f = open("out-3-labeled.dat", "w")
+            f.writelines(lines)
+            f.close()
+        if filename.startswith("4-"):
+            f = open("out-4-labeled.dat", "w")
+            f.writelines(lines)
+            f.close()
+    else:
+        perc_error = (error / nr_times) * 100
+        print("Error =", perc_error, "%")
 
 
 def main():
-    count = 0
-    for filename in files:
-        print(filename)
-        print(files_u[count])
-        dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p = index(filename)
-        prob_p2p, prob_np2p = get_prob(dic_p2p)
-        output(files_u[count], dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p)
-        count += 1
+
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    label = args['label']
+    error = args['error']
+
+    # Label mode
+    if label:
+        count = 0
+        for filename in files:
+            print(filename)
+            print(files_u[count])
+            dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p = index(filename, "label", 1)
+            prob_p2p, prob_np2p = get_prob(dic_p2p)
+            output(files_u[count], "label", dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p)
+            count += 1
+
+    # Error mode
+    if error is not None:
+        for filename in files:
+            print(filename)
+            dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, dic_p2p, missing_range = index(filename, "error", float(error))
+            prob_p2p, prob_np2p = get_prob(dic_p2p)
+            output(filename, "error", missing_range, dic_ips, dic_connection, dic_bandwidth, dic_packet_size, dic_time, prob_p2p, prob_np2p)
+
+    if not label and error is None:
+        parser.print_help()
 
 
 if __name__ == "__main__":
